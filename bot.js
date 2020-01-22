5b2ca0a79ff26c59c59b3c05eb959c658472e5b7
Aathimanirajb <34085411+Aathimanirajb@users.noreply.github.com>
2020-01-22 19:37:15 +0530

    Update bot.js
    WEBHOOK

--- bot.js
+++ bot.js
@@ -106,7 +106,8 @@ class AnswerBot extends ActivityHandler {
 
     callingExternalservice(context)
     {
-        var url = 'https://webhook.site/78f1624b-8d92-4b38-839f-3718c88d1400';
+        //var url = 'https://webhook.site/78f1624b-8d92-4b38-839f-3718c88d1400';
+        var url = 'https://salesiq.zoho.com/getsamplesuggestions.ext';
         var text = context.activity.text;
         var _body = JSON.stringify([{ 'Text': text }])
         return fetch(url, {
