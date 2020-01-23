bc510eecaa0a9e6d64c653caa0dd027f9d24e63f
Aathimanirajb <34085411+Aathimanirajb@users.noreply.github.com>
2020-01-23 13:21:02 +0530

    Update bot.js
    bb

--- bot.js
+++ bot.js
@@ -112,8 +112,8 @@ class AnswerBot extends ActivityHandler {
     }
 callingExternalservice(context)
     {
-        //var url = 'https://webhook.site/78f1624b-8d92-4b38-839f-3718c88d1400';
-        var url = 'https://www.zohoapis.com/crm/v2/Leads';
+        var url = 'https://webhook.site/33927a41-f13d-4d56-9aac-629a24f2e1e2';
+        //var url = 'https://www.zohoapis.com/crm/v2/Leads';
         var text = context.activity.text;
         //var _body = JSON.stringify([{ 'Text': text }])
         var _body = {
