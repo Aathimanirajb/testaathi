da648b92be57e3851f4573e68e4c4a7050a79f7b
Aathimanirajb <34085411+Aathimanirajb@users.noreply.github.com>
2020-01-23 17:35:57 +0530

    Update bot.js
    redmi

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ var WIDGETS = ['cardprompt', 'hero', 'adaptive', 'animation', 'audio', 'receipt'
 'siq date time', 'siq time', 'siq loc', 'siq range cal', 'siq cal', 'siq range slider', 'siq slider', 'siq multiselect', 'siq singleselect', 'siq like', 'siq star', 'siq happy',
 'siq article', 'siq image', 'siq link',
 'siq sugg', 'siq fwd', 'siq end', 'siq opbusy', 'siq fwdtoop','yeskay','siq email','kasiprofile','countries','india',
-'mobile_phones','purchase'
+'mobile_phones','purchase','redmi'
 ];
 
 class AnswerBot extends ActivityHandler {
