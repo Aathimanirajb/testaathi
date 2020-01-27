97c2eaa8df3a99faed5adf109965b57cddfc2e08
Aathimanirajb <34085411+Aathimanirajb@users.noreply.github.com>
2020-01-27 17:56:57 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ var WIDGETS = ['cardprompt', 'hero', 'adaptive', 'animation', 'audio', 'receipt'
 'siq article', 'siq image', 'siq link',
 'siq sugg', 'siq fwd', 'siq end', 'siq opbusy', 'siq fwdtoop','yeskay','siq email','kasiprofile','countries','india',
 'mobile_phones','purchase','redmi', 
-'mcorp', 'blog search', 'search by date', 'search by browsing', 'contribute', 'about mcorp', 'see def'
+'mcorp', 'blog search', 'search by date', 'search by browsing', 'contribute', 'about mcorp', 'see def',
 'endchat', 'bye'
 ];
 
